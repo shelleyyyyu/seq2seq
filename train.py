c5a2d1e7941eaaadb03e68b8e45b6a405541f0ec
Shelly Yu <shellyyu@cherrypicks.com>
2019-02-28 16:57:00 +0800

    update config

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ def add_arguments(parser):
 
     parser.add_argument("--learning_rate", type=float, default=1e-3, help="Learning rate.")
     parser.add_argument("--batch_size", type=int, default=64, help="Batch size.")
-    parser.add_argument("--num_epochs", type=int, default=25, help="Number of epochs.")
+    parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs.")
     parser.add_argument("--keep_prob", type=float, default=0.8, help="Dropout keep prob.")
 
     parser.add_argument("--toy", action="store_true", help="Use only 50K samples of data")
